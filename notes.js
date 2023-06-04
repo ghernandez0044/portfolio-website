47608a054daa90e8008389253b99aa6920d5e396
ghernandez0044 <g.hernandez0044@yahoo.com>
2023-06-04 11:10:11 -0700

    added my about section to notes.js

---
+++ notes.js
@@ -0,0 +1,2 @@
+// Hello, my name is Guillermo Hernandez I am an enthusiastic and highly motivated Full Stack Software Engineer, I find immense joy in the transformative power of software and technology. Witnessing the positive impact they have had on the world, I am deeply passionate about contributing my skills and knowledge to this ever-evolving field.
+
