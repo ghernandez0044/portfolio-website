009b8910b9c514db5d42201e68d9cba5d94ed150
ghernandez0044 <g.hernandez0044@yahoo.com>
2023-06-04 11:14:10 -0700

    added connect with me section to notes.js

--- notes.js
+++ notes.js
@@ -3,3 +3,5 @@
 // My Background I come from a technical and engineering background as I have done a lot of work in the Biomedical field with biomedical, and biotechnology companies to carry out various projects. Through my experience working in this field and my exposure to the technology and software used to power our everyday world, I gained an even greater appreciation of what software and technology can do for human beings.
 
 // My Journey I took the next step in my career and became a full stack software engineer through App Academy's rigorous full-time Coding Bootcamp where I expanded my skills on HTML, CSS, JavaScript, Node.js, Express, SQL, Sequelize, React, Redux, Python, Flask, and SQLAlchemy to continue to work in this field and have the opportunity to make this world a better place through technological advancements.
+
+// Connect With Me I am currently looking for any software engineering opportunity which will allow me to continue to grow my skills, mindset, and my impact on making this world a better place. Please reach out if you know of any opportunities that fit me or just want to connect!
