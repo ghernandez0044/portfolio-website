992d3601f68aef79081c8467ec38975f8fa3c220
ghernandez0044 <g.hernandez0044@yahoo.com>
2023-06-04 11:13:12 -0700

    added my journey section to notes.js

--- notes.js
+++ notes.js
@@ -1,3 +1,5 @@
 // Hello, my name is Guillermo Hernandez I am an enthusiastic and highly motivated Full Stack Software Engineer, I find immense joy in the transformative power of software and technology. Witnessing the positive impact they have had on the world, I am deeply passionate about contributing my skills and knowledge to this ever-evolving field.
 
 // My Background I come from a technical and engineering background as I have done a lot of work in the Biomedical field with biomedical, and biotechnology companies to carry out various projects. Through my experience working in this field and my exposure to the technology and software used to power our everyday world, I gained an even greater appreciation of what software and technology can do for human beings.
+
+// My Journey I took the next step in my career and became a full stack software engineer through App Academy's rigorous full-time Coding Bootcamp where I expanded my skills on HTML, CSS, JavaScript, Node.js, Express, SQL, Sequelize, React, Redux, Python, Flask, and SQLAlchemy to continue to work in this field and have the opportunity to make this world a better place through technological advancements.
