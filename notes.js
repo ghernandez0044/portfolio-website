675825cfbfe733bdcd55fe01f8956ee3f28c0a9b
ghernandez0044 <g.hernandez0044@yahoo.com>
2023-06-27 16:35:12 -0700

    finished infinityx favorite page

--- notes.js
+++ notes.js
@@ -5,3 +5,59 @@
 // My Journey I took the next step in my career and became a full stack software engineer through App Academy's rigorous full-time Coding Bootcamp where I expanded my skills on HTML, CSS, JavaScript, Node.js, Express, SQL, Sequelize, React, Redux, Python, Flask, and SQLAlchemy to continue to work in this field and have the opportunity to make this world a better place through technological advancements.
 
 // Connect With Me I am currently looking for any software engineering opportunity which will allow me to continue to grow my skills, mindset, and my impact on making this world a better place. Please reach out if you know of any opportunities that fit me or just want to connect!
+
+
+store = {
+    session: {
+        user
+    },
+    spacecrafts: {
+        allSpacecraft: {},
+        singleSpacecraft: {}
+    },
+    spaceports: {
+        allSpaceports: {},
+        singleSpaceport: {}
+    },
+    planets: {
+        allPlanets: {},
+        singlePlanet: {}
+    },
+    planet_comments: {
+        allPlanetComments: {},
+        singlePlanetComment: {}
+    },
+    profiles: {
+       allProfiles: {},
+       singleProfile: {}
+    },
+    wallets: {
+       allWallets: {},
+       singleWallet: {}
+    },
+    api: {
+       apod: {}
+    },
+    travelClasses: {
+       allTravelClasses: {},
+       singleTravelClass: {}
+    },
+    flights: {
+      allFlights: {},
+      singleFlight: {},
+      searchedFlights: {}
+    },
+    bookings: {
+      allBookings: {},
+      singleBooking: {},
+      userBookings: {}
+    },
+    flightBookings: {
+      currentFlightBooking: {}
+    },
+    transactions: {
+      allTransactions: {},
+      singleTransaction: {},
+      userTransactions: {}
+    }
+}
